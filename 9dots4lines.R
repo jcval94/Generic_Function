7d40f764503c6a76a161047f837b378a1956229d
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-06-15 14:39:23 +0100

    Create 9dots4lines.R
    Diferent ways to solve the problem
    9 dots 4 lines, 9 dots 3 lines, ...,  9 dots 1 line

---
+++ 9dots4lines.R
@@ -0,0 +1,25 @@
+#Planteamiento y solución con 4 línear
+
+
+#Resolveremos el problema con 3 líneas
+
+#Möbius Strip
+#Instalamos los paquetes necesarios
+install.packages("geozoo")
+install.packages("tourr")
+#Cargamos librerías
+library(geozoo)
+library(tourr)
+
+#Ejecutar mobius
+Mob<-mobius(3,200)
+#Matriz de puntos en 3D
+Mob[[1]]
+
+#Añadimos los 9 puntos del problema
+
+
+
+
+#Resolveremos el problema con 1 línea
+
